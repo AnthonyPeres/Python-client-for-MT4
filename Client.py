890351a2889800c642028c59da4acbda7d8f8a8e
Anthony Peres da Cruz <anthony.peresdacruz@gmail.com>
2020-06-10 21:18:50 +0200

    Netoyage des fonctions

--- Client.py
+++ Client.py
@@ -33,66 +33,61 @@ class Client():
             _protocol + "://" + _host + ":" + str(_PULL_PORT)
         )
 
-        self.remote_send(self.reqSocket, "Hello World !")
+        self.send_on_req("Hello Server")
 
         print("Client init successfully")
 
 
-    def remote_send(self, socket, data):
+    """
+    SEND AND RECEIVE FUNCTIONS
+    """
+
+    # On envoie une requete et on attend un ACK
+    def send_on_req(self, data):
         try: 
-            socket.send_string(data)
-            msg_send = socket.recv_string()
-            print(msg_send)
+            self.reqSocket.send_string(data)
+            message = self.reqSocket.recv_string()
+            return message
         except: 
-            print("Waiting for PUSH from MT4...")
+            print("Waiting from MT4...")
 
 
-    def remote_pull(self, socket):
+    # On receptionne un message sur la pullSocket
+    def receive_on_pull(self):
         try:
-            msg_pull = socket.recv(flags = zmq.NOBLOCK)
-            return msg_pull
+            message = self.pullSocket.recv(flags = zmq.NOBLOCK)
+            return message
         except:
-            print("Waiting for PUSH from MT4...")
+            print("Waiting from MT4...")
 
 
-    def get_data(self, symbol, timeframe, start_bar, end_bar):
-        
-        # On compose notre message (data) et on l'envoie
-        self.data = "DATA|" + symbol + "|" + "PERIOD_" + timeframe + "|" + str(start_bar) + "|" + str(end_bar + 1)
-        self.remote_send(self.reqSocket, self.data)
+    """
+    ORDER FUNCTIONS
+    """
 
-        # On recupere la réponse
-        prices = self.remote_pull(self.pullSocket)
-        prices_str = str(prices)
-        
-        price_lst = prices_str.split(sep='|')[1:-1]
-        price_lst = [float(i) for i in price_lst]
-        price_lst = price_lst[::1]
-        
-        price_arr = np.array(price_lst)
-        return price_arr
-
-    
     def buy_order(self, symbol, stop_loss, take_profit):
-        self.buy = "TRADE|OPEN|0|" + str(symbol) + "|" + str(stop_loss) + "|" + str(take_profit)
-        self.remote_send(self.reqSocket, self.buy)
-        reply = self.remote_pull(self.pullSocket)
-        return reply
+        self.send_on_req(
+            "TRADE|OPEN|0|" +
+            str(symbol) + "|" +
+            str(stop_loss) + "|" +
+            str(take_profit)
+        )
 
     def sell_order(self, symbol, stop_loss, take_profit):
-        self.buy = "TRADE|OPEN|1|" + str(symbol) + "|" + str(stop_loss) + "|" + str(take_profit)
-        self.remote_send(self.reqSocket, self.buy)
-        reply = self.remote_pull(self.pullSocket)
-        return reply
-
+        self.send_on_req(
+            "TRADE|OPEN|1|" +
+            str(symbol) + "|" +
+            str(stop_loss) + "|" + 
+            str(take_profit)
+        )
+        
     def close_buy_order(self):
-        self.close_buy = "TRADE|CLOSE|0"
-        self.remote_send(self.reqSocket, self.close_buy)
-        reply = self.remote_pull(self.pullSocket)
-        return reply
+        self.send_on_req(
+            "TRADE|CLOSE|0"
+        )
     
     def close_sell_order(self):
-        self.close_sell = "TRADE|CLOSE|1"
-        self.remote_send(self.reqSocket, self.close_sell)
-        reply = self.remote_pull(self.pullSocket)
-        return reply
+        self.send_on_req(
+            "TRADE|CLOSE|1"
+        )
+        
--- TradingStrategy.py
+++ TradingStrategy.py
@@ -1,4 +1,27 @@
 from Client import Client
-import time
+from time import sleep
 
 client = Client()
+
+"""
+Déroulement d'une boucle : 
+    1. On reçoit une valeur sur la pullSocket
+    2. On affiche cette valeur (la valeur d'une devise avec son nom etc)
+    3. On appelle la fonction de traitement avec cette valeur
+    4. On envoie un ordre si besoin
+"""
+
+while True:
+
+    # message = client.send_on_req("Salut")
+    # print(message)
+    # Quand on reçoit une valeur sur la pullSocket
+    
+    message = client.receive_on_pull()
+    print(message)
+
+    # Afficher cette valeur
+
+    # Appeler la fonction de traitement avec cette valeur
+
+    # Envoyer un ordre si besoin
